3dbcbe9de6b2f6238392e8ff2894a07a08286fbd
Geethanjal S Kumar <geethanjali2608@gmail.com>
2023-04-14 22:46:59 +0530

    g1

--- app/src/main/java/com/example/newsss/MainActivity.java
+++ app/src/main/java/com/example/newsss/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.os.RecoverySystem;
+import android.view.View;
 import android.widget.ProgressBar;
 
 import java.util.ArrayList;
@@ -36,22 +37,28 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
         newsRV.setLayoutManager(new LinearLayoutManager(this));
         newsRV.setAdapter(newsRVAdapter);
         categoryRV.setAdapter(categoryRVAdapter);
+        getCategories();
 
 
 
 
     }
-
-//klhjdh lkhjkh
     private void getCategories(){
-        categoryRVModalArrayList.add(new CategoryRVModal("All","") );
-        categoryRVModalArrayList.add(new CategoryRVModal("Technology","") );
-        categoryRVModalArrayList.add(new CategoryRVModal("Science","") );
-        categoryRVModalArrayList.add(new CategoryRVModal("Sports","") );
-        categoryRVModalArrayList.add(new CategoryRVModal("General","") );
-        categoryRVModalArrayList.add(new CategoryRVModal("Business","") );
-        categoryRVModalArrayList.add(new CategoryRVModal("Entertainment","") );
-        categoryRVModalArrayList.add(new CategoryRVModal("Health","") );
+        categoryRVModalArrayList.add(new CategoryRVModal("All","https://images.unsplash.com/photo-1676027274842-6195f8cdace7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDV8QkpKTXR0ZURKQTR8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60") );
+        categoryRVModalArrayList.add(new CategoryRVModal("Technology","https://images.unsplash.com/photo-1676027274842-6195f8cdace7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDV8QkpKTXR0ZURKQTR8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60") );
+        categoryRVModalArrayList.add(new CategoryRVModal("Science","https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHNjaWVuY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60") );
+        categoryRVModalArrayList.add(new CategoryRVModal("Sports","https://images.unsplash.com/photo-1541252260730-0412e8e2108e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNwb3J0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60") );
+        categoryRVModalArrayList.add(new CategoryRVModal("General","https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2VuZXJhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60") );
+        categoryRVModalArrayList.add(new CategoryRVModal("Business","https://images.unsplash.com/photo-1610374792793-f016b77ca51a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YnVpc25lc3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60") );
+        categoryRVModalArrayList.add(new CategoryRVModal("Entertainment","https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZW50ZXJ0YWlubWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60") );
+        categoryRVModalArrayList.add(new CategoryRVModal("Health","https://images.unsplash.com/photo-1535914254981-b5012eebbd15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGhlYWx0aHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60") );
+        categoryRVAdapter.notifyDataSetChanged();
+
+    }
+    private void getNews(String category){
+        loadingPB.setVisibility(View.VISIBLE);
+        articlesArrayList.clear();
+        String categoryURL="https://newsapi.org/v2/top-headlines?country=in&category="+category+"&apiKey=452ea4d140ef4ca29a771b76d33f225c";
     }
     @Override
     public void onCategoryClick(int position) {
